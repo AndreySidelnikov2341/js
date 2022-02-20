45792c9d3b8e4e1f6b3ceb1d9c988c038f0f4a88
AndreySidelnikov <andron2341991@gmail.com>
2022-02-20 17:34:50 +0200

    fourth_task

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,30 @@ var userObj = {
   age: 30,
   firstName: 'Anton',
   lastName: 'Ivanov',
+  fullName() {
+    return `${userObj.firstName} ${userObj.lastName}`;
+  }
 };
 
 console.log(userObj);
+console.log(userObj.fullName());
 
+function defUpperStr(str) {
+  return (str || 'Default text').toUpperCase();
+}
+console.log(defUpperStr('My text'));
+console.log(defUpperStr());
 
+function evenFn(n) {
+  let arr = [];
+
+  for (let i = 1; i <= n; i++) if (i % 2 === 0) arr.push(i);
+
+  return arr;
+}
+
+console.log(evenFn(10));
+console.log(evenFn(15));
+console.log(evenFn(20));
 
 
